cfe3d917f3005724136edfddace7b99a06706d74
Ahmed Amin <ahmedamin@gmail.com>
2024-02-10 02:24:29 +0200

    update

--- src/components/common/userAndOrderScreen/userAndOrderPage.jsx
+++ src/components/common/userAndOrderScreen/userAndOrderPage.jsx
@@ -4,7 +4,7 @@ import ShowProductPage from "../oneProduct/showOneProduct";
 
 const UserAndOrderPage = () => {
   return (
-    <div className="">
+    <div className="bg-itemsBgLight dark:bg-itemsBgDark rounded-lg">
       <div className="container mx-auto py-8">
         <div className="grid grid-cols-4 sm:grid-cols-12 gap-6 px-4">
           <div className="col-span-4 sm:col-span-3">
