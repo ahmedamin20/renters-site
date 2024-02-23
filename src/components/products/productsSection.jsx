1a839e0dded6419178363348f2968abe669e90d3
Ahmed Amin <ahmedamin@gmail.com>
2024-02-23 18:22:02 +0200

    updateProducts

--- src/components/products/productsSection.jsx
+++ src/components/products/productsSection.jsx
@@ -1,28 +1,23 @@
-import React from 'react'
-import ProductCard from './productCard'
-import { useQuery } from '@tanstack/react-query'
-import { API_ENDPOINTS } from '../../utils/config/constants';
-import { getAllProducts } from '../../api/queries/products';
+import React from "react";
+import ProductCard from "./productCard";
+import { useQuery } from "@tanstack/react-query";
+import { API_ENDPOINTS } from "../../utils/config/constants";
+import { getAllProducts } from "../../api/queries/products";
 
 const ProductsSection = () => {
-    const {data,status} = useQuery({
-        queryKey:[API_ENDPOINTS.PRODUCTS],
-        queryFn:getAllProducts
-      })
-
-
-    
-
+  const { data } = useQuery({
+    queryKey: [API_ENDPOINTS.PRODUCTS],
+    queryFn: getAllProducts,
+  });
   return (
-    <div className='flex justify-evenly flex-wrap w-full'>
-      {data?.data.map(item=>(
-        <div className='w-52' >
-        <ProductCard key={item.id} product={item}/>
+    <div className="flex justify-evenly flex-wrap w-full">
+      {data?.data.map((item) => (
+        <div className="w-52">
+          <ProductCard key={item.id} product={item} />
         </div>
       ))}
-
     </div>
-  )
-}
+  );
+};
 
-export default ProductsSection
+export default ProductsSection;
