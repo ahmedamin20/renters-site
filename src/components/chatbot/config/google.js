a4e79fc75bafe2bf3f22eb8940a7a972a2885f4c
AhmedAmin <ahmedamin01147@hotmail.com>
2024-05-24 02:11:03 +0300

    update

--- src/components/chatbot/config/google.js
+++ src/components/chatbot/config/google.js
@@ -1,13 +1,12 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-const secret = "AIzaSyB_ZnVsOGDChmx59CCOBoUPup_LET57nd4"
 
-const genAI = new GoogleGenerativeAI(secret);
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_SECRET);
   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
   export async function GoogleAi(prompt) {
   
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = await response.text();
-    console.log(text)
     return  text;
+    
   }
--- src/components/chatbot/config/messageProvider.js
+++ src/components/chatbot/config/messageProvider.js
@@ -1,20 +1,17 @@
-import React from 'react'
-import { GoogleAi } from './google';
+import { GoogleAi } from "./google";
 
 const MessageProvider = (createChatBotMessage, setState) => {
-    const handleHello = async (message) => {
-        const botMessage = await createChatBotMessage( await GoogleAi(message));
-        console.log(botMessage)
-        setState((prev) => ({
-          ...prev,
-          messages: [...prev.messages, botMessage],
-        }));
-      };
+  const handleHello = async (message) => {
+    const botMessage = await createChatBotMessage(await GoogleAi(message));
+    console.log(botMessage);
+    setState((prev) => ({
+      ...prev,
+      messages: [...prev.messages, botMessage],
+    }));
+  };
   return {
-    handleHello
+    handleHello,
+  };
+};
 
-  }
-  
-}
-
-export default MessageProvider
+export default MessageProvider;
