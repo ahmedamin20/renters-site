8968c1322bf2b6a442c359588fbe6ea4341f2563
AhmedAmin <ahmedamin01147@hotmail.com>
2024-06-22 20:36:20 +0300

    update

--- src/components/home/shopByCatigory/shopByCatigorySection.jsx
+++ src/components/home/shopByCatigory/shopByCatigorySection.jsx
@@ -27,11 +27,11 @@ const ShopByCategory = () => {
               <img
                 className="object-center object-cover h-full w-full"
                 // src="https://i.ibb.co/ThPFmzv/omid-armin-m-VSb6-PFk-VXw-unsplash-1-1.png"
-                src={category[4]?.image}
+                src={category[3]?.image}
                 alt="girl-image"
               />
-              <Link to={`/products/category/${category[4]?.id}`} onClick={()=> console.log(category[4]?.id)} className="dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bottom-4 z-10 absolute text-base font-medium leading-none text-gray-800 py-3 w-36 bg-white">
-                {category[4]?.name}
+              <Link to={`/products/category/${category[3]?.id}`} onClick={()=> console.log(category[3]?.id)} className="dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bottom-4 z-10 absolute text-base font-medium leading-none text-gray-800 py-3 w-36 bg-white">
+                {category[3]?.name}
               </Link>
               <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50"></div>
             </div>
