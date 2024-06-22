be9fc815d0c372e2e3c0b7d691a060649ce75877
AhmedAmin <ahmedamin01147@hotmail.com>
2024-06-22 04:38:22 +0300

    fix

--- src/components/popupForm.jsx
+++ src/components/popupForm.jsx
@@ -14,11 +14,13 @@ const Modal = ({disable, children, action, text, innerBtnText, onClick, classNam
       toggleModal()
     })
   }
+
+  // ${innerBtnText == "Verify" && "hidden"}
   return (
     <>
       <button
         onClick={toggleModal}
-        className={cn(`block text-white ${innerBtnText == "Verify" && "hidden"} bg-primary transition-all  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`, className)}
+        className={cn(`block text-white  bg-primary transition-all  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`, className)}
         type="button"
       >
         {text}
